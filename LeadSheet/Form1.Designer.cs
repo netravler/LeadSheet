54edc0a4c8ecbf3f1fcfc5bc36105386ad5e19b9
netravler <pheintz@hotmail.com>
2018-09-13 13:26:38 -0500

    Add files via upload

---
+++ LeadSheet/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace LeadSheet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblImportPDF = new System.Windows.Forms.Label();
+            this.tbImportPDF = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(591, 28);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(44, 71);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(744, 511);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // lblImportPDF
+            // 
+            this.lblImportPDF.AutoSize = true;
+            this.lblImportPDF.Location = new System.Drawing.Point(41, 15);
+            this.lblImportPDF.Name = "lblImportPDF";
+            this.lblImportPDF.Size = new System.Drawing.Size(60, 13);
+            this.lblImportPDF.TabIndex = 2;
+            this.lblImportPDF.Text = "Import PDF";
+            // 
+            // tbImportPDF
+            // 
+            this.tbImportPDF.Location = new System.Drawing.Point(44, 31);
+            this.tbImportPDF.Name = "tbImportPDF";
+            this.tbImportPDF.Size = new System.Drawing.Size(336, 20);
+            this.tbImportPDF.TabIndex = 3;
+            this.tbImportPDF.DoubleClick += new System.EventHandler(this.tbImportPDF_DoubleClick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(828, 607);
+            this.Controls.Add(this.tbImportPDF);
+            this.Controls.Add(this.lblImportPDF);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Lead Sheet - V1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblImportPDF;
+        private System.Windows.Forms.TextBox tbImportPDF;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ LeadSheet/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace LeadSheet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        static string PrettyXml(string xml)
+        {
+            var stringBuilder = new StringBuilder(); 
+            var element = XElement.Parse(xml); 
+            var settings = new XmlWriterSettings();
+  
+            settings.OmitXmlDeclaration = true;
+            settings.Indent = true;  
+            settings.NewLineOnAttributes = true; 
+
+            using (var xmlWriter = XmlWriter.Create(stringBuilder, settings))
+            {
+                element.Save(xmlWriter);
+            }
+
+            return stringBuilder.ToString();}
+
+        private void tbImportPDF_DoubleClick(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+                tbImportPDF.Text = openFileDialog1.FileName;
+            }
+        } 
+    }
+}
